55cb3b3780448f7ee5493bc3181133a0065306d0
Almira <almiratostova@gmail.com>
2026-05-14 18:21:19 +0200

    Added model User.js

---
+++ practicum_6/models/Users.js
@@ -0,0 +1,42 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 18,
+        max: 120,
+      },
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: false,
+  },
+);
+export default User;
