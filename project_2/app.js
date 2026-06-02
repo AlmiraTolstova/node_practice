67dd4f412e81b48f890a1cdc882adba55dd35d79
Almira <almiratostova@gmail.com>
2026-06-02 09:30:58 +0200

    Onbording mini project_2

---
+++ project_2/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+const app = express();
+
+const dbURI = process.env.MONGO_URI || "url";
+const port = process.env.PORT || 3000;
+
+startServer();
+
+async function startServer() {
+  try {
+    await mongoose.connect(dbURI);
+
+    console.log("Successfully connected to MongoDB!");
+    app.listen(port, () => {
+      console.log(`Server started at http://127.0.0.1:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
