2a2e82ddd1192bb15f03c97df6d8cf0a712a9b53
Almira <almiratostova@gmail.com>
2026-06-09 09:27:23 +0200

    project structure

---
+++ project_3/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+const app = express();
+
+const dbURI = process.env.MONGO_URI || "url";
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+connectDB();
+
+startServer();
+
+async function startServer() {
+  try {
+    await mongoose.connect(dbURI);
+
+    console.log("Successfully connected to MongoDB!");
+    app.listen(port, () => {
+      console.log(`Server started at http://127.0.0.1:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// app.listen(port, () => {
+//   console.log(`Server started at http://127.0.0.1:${port}`);
+// });
